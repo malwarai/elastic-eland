9fdccee4cbf52ae15b37e9a0b2081ffe1008e9e3
Aitor Arronte <aitoraalvarez@gmail.com>
2021-12-08 20:25:23 -1000

    added changes to graph generation method

--- event_data.py
+++ event_data.py
@@ -15,12 +15,17 @@ def get_event_data( index, eql_query=None, size=1000):
 
 
 #Create graph from event data based on ip of the source
-def generate_event_graph(df):
+def generate_event_graph_apache(df):
 	G = nx.Graph()
-	hosts = df['host.name']
+	hosts = list(df['host.name'].unique())
 	k=1
+	for h in hosts:
+		G.add_node(k, type='host', hostname = h, color='blue')
+		k+=1
 	for d in df.iterrows():
-		G.add_node(k, type='source', ip= d[1]['source.ip'])
+		G.add_node(k, type='source', ip= d[1]['source.ip'], color='yellow', country=d[1]['source.geo.country_name'], response = d[1]['http.response.status_code'], referrer=d[1]['http.request.referrer'])
+		G.add_edges_from([(k, hosts.index(d[1]['host.name'])+1)])
+		nx.set_edge_attributes(G, {(k, hosts.index(d[1]['host.name'])+1): {"weight": 2.0}})
 		k+=1
 	return G
 
