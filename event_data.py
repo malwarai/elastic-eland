8aa4581e7332031c2fc7a8c49a25fbfb05738722
Aitor Arronte <aitoraalvarez@gmail.com>
2021-12-27 23:48:47 -1000

    added methods to determine severity of a potential attack

--- event_data.py
+++ event_data.py
@@ -15,7 +15,7 @@ def get_event_data( index, eql_query=None, size=1000):
 
 
 #Create graph from event data based on ip of the source
-def generate_event_graph_apache(df):
+def generate_event_graph_apache_errors(df):
 	G = nx.Graph()
 	hosts = list(df['host.name'].unique())
 	k=1
@@ -23,15 +23,27 @@ def generate_event_graph_apache(df):
 		G.add_node(k, type='host', hostname = h, color='blue')
 		k+=1
 	for d in df.iterrows():
-		G.add_node(k, type='source', ip= d[1]['source.ip'], color='yellow', country=d[1]['source.geo.country_name'], response = d[1]['http.response.status_code'], referrer=d[1]['http.request.referrer'])
+		G.add_node(k, type='source', ip= d[1]['source.ip'], color='yellow', country=d[1]['source.geo.country_name'], \
+		           response = d[1]['http.response.status_code'], referrer=d[1]['http.request.referrer'])
 		G.add_edges_from([(k, hosts.index(d[1]['host.name'])+1)])
-		nx.set_edge_attributes(G, {(k, hosts.index(d[1]['host.name'])+1): {"weight": 2.0}})
+		newData = df[(df['source.ip'] == d[1]['source.ip']) & (df['host.name'] ==d[1]['host.name'])]
+		severity = attack_severity(newData)
+		nx.set_edge_attributes(G, {(k, hosts.index(d[1]['host.name'])+1): {"severity": severity}})
 		k+=1
 	return G
 
 
+#define attack severity based on the url pattern of the host and the number of cases
+def attack_severity(data):
+	count_cases = data.shape[0]
+	unique_url_paths =list(data['url.path'].unique())
+	score = [find_vulnerability_indicator(u) for u in unique_url_paths]
+	return sum(score)
 
 
-
-
+def find_vulnerability_indicator(indicator):
+	# TODO use an API to determine how the indicator is related to an existing vuln, e.g. CVE
+	# severity should be related to the CVSS or other vuln system
+	severity=0.01
+	return severity
 
---
+++ main.py
@@ -0,0 +1,5 @@
+from event_data import (get_event_data, generate_event_graph_apache)
+
+
+if __name__ == 'main':
+	data = get_event_data()
